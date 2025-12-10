7fb743e7d58318a42bc0869850b9eb75e94d354c
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-10 11:36:40 -0800

    Update Sidebar.tsx

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 export default function Sidebar(): React.ReactElement<nav> {
   return (
     <nav 
-      className="float-left w-1/5 
+      className="float-left w-1/5 mr-20 
                  bg-sky-800 
                  border-solid border-r-4 border-cyan-400"
     />;
