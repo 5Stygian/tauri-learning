0b4ca04774d10948a47a062be67dfa95f414f044
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 12:41:36 -0800

    Update periodicTable.tsx

--- src/app/components/periodicTable/periodicTable.tsx
+++ src/app/components/periodicTable/periodicTable.tsx
@@ -6,7 +6,7 @@ export default function PeriodicTable() {
     <div className="grid grid-cols-18 grid-rows-7 gap-2">
       <EmptyCell />
       <Element symbol='H' name='Hydrogen' atomicNumber={1} color="gray-400" />
-      <EmptyCell />
+      <HydrogenHeliumDist />
       <Element symbol='He' name='Helium' atomicNumber={2} color="gray-400" />
       <EmptyCell />
     </div>
