dfaa69c9c442b15789939bb64c1ebf2e23f4606c
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 12:06:21 -0800

    create EmptyCell

--- src/app/components/periodicTable/elements/element.tsx
+++ src/app/components/periodicTable/elements/element.tsx
@@ -6,7 +6,6 @@ interface ElementProps {
   
   // display
   color: string;
-  empty: boolean;
 }
 
 export default function Element({
@@ -14,21 +13,18 @@ export default function Element({
   name,
   atomicNumber,
   color,
-  empty = false,
 }: ElementProps) {
-  if (!empty) {
-    const blockStyle = `w-25 h-25 border-2 shadow-2x1 shadow-${color} text-${color}`;
-  
-    return (
-      <div className={blockStyle}>
-        <span className="text-lg pl-1.5">{atomicNumber}</span>
-        <div className="text-center text-5xl -mt-2">{symbol}</div>
-        <div className="text-center">{name}</div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="w-25 h-25 opacity-0"></div>
-    );
-  }
+  const blockStyle = `w-25 h-25 border-2 shadow-2x1 shadow-${color} text-${color}`;
+
+  return (
+    <div className={blockStyle}>
+      <span className="text-lg pl-1.5">{atomicNumber}</span>
+      <div className="text-center text-5xl -mt-2">{symbol}</div>
+      <div className="text-center">{name}</div>
+    </div>
+  );
+}
+
+export function EmptyCell() {
+  return ( <div className="w-25 h-25"></div> );
 }
