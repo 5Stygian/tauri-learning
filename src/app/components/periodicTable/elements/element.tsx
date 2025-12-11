27080bac75921bfabb9db81883c14cb9bfcc8a33
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 12:02:55 -0800

    Update element.tsx

--- src/app/components/periodicTable/elements/element.tsx
+++ src/app/components/periodicTable/elements/element.tsx
@@ -5,7 +5,8 @@ interface ElementProps {
   atomicNumber: number;
   
   // display
-  color: string; 
+  color: string;
+  empty: boolean;
 }
 
 export default function Element({
@@ -13,14 +14,21 @@ export default function Element({
   name,
   atomicNumber,
   color,
+  empty = false,
 }: ElementProps) {
-  const blockStyle = `w-25 h-25 border-2 shadow-2x1 shadow-${color} text-${color}`;
-
-  return (
-    <div className={blockStyle}>
-      <span className="text-lg pl-1.5">{atomicNumber}</span>
-      <div className="text-center text-5xl -mt-2">{symbol}</div>
-      <div className="text-center">{name}</div>
-    </div>
-  );
+  if (!empty) {
+    const blockStyle = `w-25 h-25 border-2 shadow-2x1 shadow-${color} text-${color}`;
+  
+    return (
+      <div className={blockStyle}>
+        <span className="text-lg pl-1.5">{atomicNumber}</span>
+        <div className="text-center text-5xl -mt-2">{symbol}</div>
+        <div className="text-center">{name}</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-25 h-25 opacity-0"></div>
+    );
+  }
 }
