545398575ff3e7fc2c339d97d6821c88ca581fc7
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 12:07:31 -0800

    Update periodicTable.tsx

--- src/app/components/periodicTable/periodicTable.tsx
+++ src/app/components/periodicTable/periodicTable.tsx
@@ -1,11 +1,13 @@
 import { HasChildren } from '../../util/types';
-import Element from './elements/element';
+import Element, { EmptyCell } from './elements/element';
 
 export default function PeriodicTable({ children }: HasChildren) {
   return (
     <div className="grid grid-cols-18 grid-rows-7 gap-2">
-      <Element empty={true} />
+      <EmptyCell />
       <Element symbol='H' name='Hydrogen' atomicNumber={1} color="gray-400" />
+      <EmptyCell />
+      <Element symbol='He' name='Helium' atomicNumber={2} color="gray-400" />
       {children}
     </div>
   );
