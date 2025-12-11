ea185c34019a9377a641242145d1d5bc08f81138
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 12:39:23 -0800

    Update emptyCell.tsx

--- src/app/components/periodicTable/elements/emptyCell.tsx
+++ src/app/components/periodicTable/elements/emptyCell.tsx
@@ -1,3 +1,3 @@
 export default function EmptyCell() {
-  return ( <div className="min-w-38 min-h-38 border-red-400 border-4"></div> );
+  return ( <div className="min-w-25 min-h-25 border-red-400 border-4"></div> );
 }
