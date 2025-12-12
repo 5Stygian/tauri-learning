f0e4b59734ff179d8eb8d0feeef86d204b543f63
5Stygian <rlcrafistohard@gmail.com>
2025-12-12 00:15:53 -0800

    fix incorrect atomic numbers

--- src/app/components/periodicTable/periods/actinides.tsx
+++ src/app/components/periodicTable/periods/actinides.tsx
@@ -6,21 +6,21 @@ export default function Actinides() {
   return (
     <>
       <Cell empty={true} amount={3} />
-      <Cell symbol="Ac" name="Actinium" atomicNumber={57} color={config.colors.actinide} />
-      <Cell symbol="Th" name="Thorium" atomicNumber={58} color={config.colors.actinide} />
-      <Cell symbol="Pa" name="Protactinium" atomicNumber={59} color={config.colors.actinide} />
-      <Cell symbol="U" name="Uranium" atomicNumber={60} color={config.colors.actinide} />
-      <Cell symbol="Np" name="Neptunium" atomicNumber={61} color={config.colors.actinide} />
-      <Cell symbol="Pu" name="Plutonium" atomicNumber={62} color={config.colors.actinide} />
-      <Cell symbol="Am" name="Americium" atomicNumber={63} color={config.colors.actinide} />
-      <Cell symbol="Cm" name="Curium" atomicNumber={64} color={config.colors.actinide} />
-      <Cell symbol="Bk" name="Berkelium" atomicNumber={65} color={config.colors.actinide} />
-      <Cell symbol="Cf" name="Californium" atomicNumber={66} color={config.colors.actinide} />
-      <Cell symbol="Es" name="Einsteinium" atomicNumber={67} color={config.colors.actinide} />
-      <Cell symbol="Fm" name="fermium" atomicNumber={68} color={config.colors.actinide} />
-      <Cell symbol="Md" name="Mendelevium" atomicNumber={69} color={config.colors.actinide} />
-      <Cell symbol="No" name="Nobelium" atomicNumber={70} color={config.colors.actinide} />
-      <Cell symbol="Lr" name="Lawrencium" atomicNumber={71} color={config.colors.actinide} />
+      <Cell symbol="Ac" name="Actinium" atomicNumber={89} color={config.colors.actinide} />
+      <Cell symbol="Th" name="Thorium" atomicNumber={90} color={config.colors.actinide} />
+      <Cell symbol="Pa" name="Protactinium" atomicNumber={91} color={config.colors.actinide} />
+      <Cell symbol="U" name="Uranium" atomicNumber={92} color={config.colors.actinide} />
+      <Cell symbol="Np" name="Neptunium" atomicNumber={93} color={config.colors.actinide} />
+      <Cell symbol="Pu" name="Plutonium" atomicNumber={94} color={config.colors.actinide} />
+      <Cell symbol="Am" name="Americium" atomicNumber={95} color={config.colors.actinide} />
+      <Cell symbol="Cm" name="Curium" atomicNumber={96} color={config.colors.actinide} />
+      <Cell symbol="Bk" name="Berkelium" atomicNumber={97} color={config.colors.actinide} />
+      <Cell symbol="Cf" name="Californium" atomicNumber={98} color={config.colors.actinide} />
+      <Cell symbol="Es" name="Einsteinium" atomicNumber={99} color={config.colors.actinide} />
+      <Cell symbol="Fm" name="fermium" atomicNumber={100} color={config.colors.actinide} />
+      <Cell symbol="Md" name="Mendelevium" atomicNumber={101} color={config.colors.actinide} />
+      <Cell symbol="No" name="Nobelium" atomicNumber={102} color={config.colors.actinide} />
+      <Cell symbol="Lr" name="Lawrencium" atomicNumber={103} color={config.colors.actinide} />
     </>
   );
 }
--- src/app/components/periodicTable/periods/period7.tsx
+++ src/app/components/periodicTable/periods/period7.tsx
@@ -5,24 +5,24 @@ import config from "../../../data/config.ts";
 export default function Period7() {
   return (
     <>
-      <Cell symbol="Fr" name="Francium" atomicNumber={19} color={config.colors.alkaliMetal} />
-      <Cell symbol="Ra" name="Radium" atomicNumber={20} color={config.colors.alkalineEarthMetal} />
+      <Cell symbol="Fr" name="Francium" atomicNumber={88} color={config.colors.alkaliMetal} />
+      <Cell symbol="Ra" name="Radium" atomicNumber={87} color={config.colors.alkalineEarthMetal} />
       <Cell compressedFBlock={true} name="Actinide Series" color={config.colors.actinide} />
-      <Cell symbol="Rf" name="Rutherfordium" atomicNumber={23} color={config.colors.transitionMetal} />
-      <Cell symbol="Db" name="Dubnium" atomicNumber={24} color={config.colors.transitionMetal} />
-      <Cell symbol="Sg" name="Seaborgium" atomicNumber={24} color={config.colors.transitionMetal} />
-      <Cell symbol="Bh" name="Bohrium" atomicNumber={25} color={config.colors.transitionMetal} />
-      <Cell symbol="Hs" name="Hassium" atomicNumber={26} color={config.colors.transitionMetal} />
-      <Cell symbol="Mt" name="Meitnerium" atomicNumber={27} color={config.colors.transitionMetal} />
-      <Cell symbol="Ds" name="Darmstadtium" atomicNumber={28} color={config.colors.transitionMetal} />
-      <Cell symbol="Rg" name="Roentgenium" atomicNumber={29} color={config.colors.transitionMetal} />
-      <Cell symbol="Cn" name="Copernicium" atomicNumber={30} color={config.colors.transitionMetal} />
-      <Cell symbol="Nh" name="Nihonium" atomicNumber={31} color={config.colors.postTransitionMetal} />
-      <Cell symbol="Fl" name="Flerovium" atomicNumber={32} color={config.colors.postTransitionMetal} />
-      <Cell symbol="Mc" name="Moscovium" atomicNumber={33} color={config.colors.postTransitionMetal} />
-      <Cell symbol="Lv" name="Livermorium" atomicNumber={34} color={config.colors.postTransitionMetal} />
-      <Cell symbol="Ts" name="Tennessine" atomicNumber={35} color={config.colors.halogen} />
-      <Cell symbol="Og" name="Oganesson" atomicNumber={36} color={config.colors.nobleGas} />
+      <Cell symbol="Rf" name="Rutherfordium" atomicNumber={104} color={config.colors.transitionMetal} />
+      <Cell symbol="Db" name="Dubnium" atomicNumber={105} color={config.colors.transitionMetal} />
+      <Cell symbol="Sg" name="Seaborgium" atomicNumber={106} color={config.colors.transitionMetal} />
+      <Cell symbol="Bh" name="Bohrium" atomicNumber={107} color={config.colors.transitionMetal} />
+      <Cell symbol="Hs" name="Hassium" atomicNumber={108} color={config.colors.transitionMetal} />
+      <Cell symbol="Mt" name="Meitnerium" atomicNumber={109} color={config.colors.transitionMetal} />
+      <Cell symbol="Ds" name="Darmstadtium" atomicNumber={110} color={config.colors.transitionMetal} />
+      <Cell symbol="Rg" name="Roentgenium" atomicNumber={111} color={config.colors.transitionMetal} />
+      <Cell symbol="Cn" name="Copernicium" atomicNumber={112} color={config.colors.transitionMetal} />
+      <Cell symbol="Nh" name="Nihonium" atomicNumber={113} color={config.colors.postTransitionMetal} />
+      <Cell symbol="Fl" name="Flerovium" atomicNumber={114} color={config.colors.postTransitionMetal} />
+      <Cell symbol="Mc" name="Moscovium" atomicNumber={115} color={config.colors.postTransitionMetal} />
+      <Cell symbol="Lv" name="Livermorium" atomicNumber={116} color={config.colors.postTransitionMetal} />
+      <Cell symbol="Ts" name="Tennessine" atomicNumber={117} color={config.colors.halogen} />
+      <Cell symbol="Og" name="Oganesson" atomicNumber={118} color={config.colors.nobleGas} />
     </>
   );
 }
