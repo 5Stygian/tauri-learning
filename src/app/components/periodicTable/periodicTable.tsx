9f14ce8f5ed281cbd7274ac87ba537560f263399
5Stygian <rlcrafistohard@gmail.com>
2025-12-11 20:28:29 -0900

    finish period 7

--- src/app/components/periodicTable/periodicTable.tsx
+++ src/app/components/periodicTable/periodicTable.tsx
@@ -3,8 +3,8 @@ import Period2 from "./periods/period2";
 import Period3 from "./periods/period3";
 import Period4 from "./periods/period4";
 import Period5 from "./periods/period5";
-//import Period6 from "./periods/period6";
-//import Period7 from "./periods/period7";
+import Period6 from "./periods/period6";
+import Period7 from "./periods/period7";
 
 export default function PeriodicTable() {
   return (
@@ -14,6 +14,8 @@ export default function PeriodicTable() {
       <Period3 />
       <Period4 />
       <Period5 />
+      <Period6 />
+      <Period7 />
     </div>
   );
 }
---
+++ src/app/components/periodicTable/periods/period6.tsx
@@ -0,0 +1,28 @@
+import Cell from "../cell/cell.tsx"
+
+import config from "../../../data/config.ts";
+
+export default function Period5() {
+  return (
+    <>
+      <Cell symbol="Cs" name="Cesium" atomicNumber={55} color={config.colors.alkaliMetal}/>
+      <Cell symbol="Ba" name="Barium" atomicNumber={56} color={config.colors.alkalineEarthMetal}/>
+      <Cell />
+      <Cell symbol="Hf" name="Hafnium" atomicNumber={72} color={config.colors.transitionMetal}/>
+      <Cell symbol="Ta" name="Tantalum" atomicNumber={73} color={config.colors.transitionMetal}/>
+      <Cell symbol="W" name="Tungsten" atomicNumber={74} color={config.colors.transitionMetal}/>
+      <Cell symbol="Re" name="Rhenium" atomicNumber={75} color={config.colors.transitionMetal}/>
+      <Cell symbol="Os" name="Osmium" atomicNumber={76} color={config.colors.transitionMetal}/>
+      <Cell symbol="Ir" name="Iridium" atomicNumber={77} color={config.colors.transitionMetal}/>
+      <Cell symbol="Pt" name="Platinum" atomicNumber={78} color={config.colors.transitionMetal}/>
+      <Cell symbol="Au" name="Gold" atomicNumber={79} color={config.colors.transitionMetal}/>
+      <Cell symbol="Hg" name="Mercury" atomicNumber={80} color={config.colors.transitionMetal}/>
+      <Cell symbol="Tl" name="Thallium" atomicNumber={81} color={config.colors.postTransitionMetal}/>
+      <Cell symbol="Pb" name="Lead" atomicNumber={82} color={config.colors.postTransitionMetal}/>
+      <Cell symbol="Bi" name="Bismuth" atomicNumber={83} color={config.colors.postTransitionMetal}/>
+      <Cell symbol="Po" name="Polonium" atomicNumber={84} color={config.colors.metalloid}/>
+      <Cell symbol="At" name="Astatine" atomicNumber={85} color={config.colors.halogen}/>
+      <Cell symbol="Rn" name="Radon" atomicNumber={86} color={config.colors.nobleGas}/>
+    </>
+  );
+}
