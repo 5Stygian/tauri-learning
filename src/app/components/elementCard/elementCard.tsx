0037ead5344a18db5017985dac408d9557fbe456
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-16 11:19:14 -0800

    Update elementCard.tsx

--- src/app/components/elementCard/elementCard.tsx
+++ src/app/components/elementCard/elementCard.tsx
@@ -2,7 +2,7 @@ import { NaE } from "@/app/data/elements";
 
 export default function ElementCard() {
   const symbolStyling = `min-w-32 w-min min-h-40 bg-black border-3 shadow-2x1 font-bold ${NaE.color} flex justify-center items-center hover:cursor-default`;
-  const dataTableStyling = `min-w-220 w-min min-h-40 bg-black border-3 shadow-2x1 font-bold ${NaE.color} flex justify-center items-center ml-8 px-8 py-6 hover:cursor-default`;
+  const dataTableStyling = `basis-3xs w-min min-h-40 bg-black border-3 shadow-2x1 font-bold ${NaE.color} flex justify-center items-center ml-8 px-8 py-6 hover:cursor-default`;
   const dataTitleStyling = "pr-12 float-left text-nowrap";
   const dataContentStyling = "text-white";
   
