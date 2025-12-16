fe742b5f411182358df11ee8afb673353dad3b06
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-16 11:55:40 -0800

    Create sourceCode.tsx

---
+++ src/app/components/misc/sourceCode.tsx
@@ -0,0 +1,7 @@
+export default function SourceCode() {
+  return (
+    <div className="pl-4">
+      This website is open source. The source code can be found on <a href="https://github.com/5Stygian/stygian-periodic-table/" target="_blank">GitHub</a>
+    </div>
+  );
+}
