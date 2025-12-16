d6ff68051aa5d873e17fa6832698d3750912a358
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-16 11:56:18 -0800

    Update index.ts

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,5 +3,6 @@ import Sidebar, { SidebarElement } from "./sidebar/Sidebar";
 import PeriodicTable from "./periodicTable/periodicTable";
 import ElementCard from "./elementCard/elementCard";
 import License from "./misc/license";
+import SourceCode from "./misc/sourceCode";
 
-export { MainPage, Sidebar, SidebarElement, PeriodicTable, ElementCard, License };
+export { MainPage, Sidebar, SidebarElement, PeriodicTable, ElementCard, License, SourceCode };
