8a3899041107028f10f2b02b15d5a5afb1aa93a4
5Stygian <rlcrafistohard@gmail.com>
2025-12-17 13:28:54 -0900

    make text white so light mode troons can experience the website in its true glory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import * as c from './components/index';
 
 export default function Index() {
   return (
-    <main className="min-h-screen w-full bg-zinc-950">
+    <main className="min-h-screen w-full bg-zinc-950 text-white">
       <c.MainPage>
         <c.PeriodicTable />
         <c.ElementCard />
