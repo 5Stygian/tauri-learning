e7aa5126e1c78107a77034d2d4281fe152af7774
5Stygian <rlcrafistohard@gmail.com>
2025-12-16 23:05:16 -0800

    this fuckass import bro

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,6 +3,6 @@ import PeriodicTable from "./periodicTable/periodicTable";
 import ElementCard from "./elementCard/elementCard";
 import License from "./misc/license";
 import SourceCode from "./misc/sourceCode";
-import Sidebar from "./sidebar/sidebar";
+import Sidebar from "./sidebar/Sidebar";
 
 export { MainPage, Sidebar, PeriodicTable, ElementCard, License, SourceCode };
