0fe10572ea58c097603458fc248322de357f0d44
5Stygian <rlcrafistohard@gmail.com>
2025-12-19 14:17:38 -0900

    change import path

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,6 +3,6 @@ import PeriodicTable from "./periodicTable/periodicTable";
 import ElementCard from "./elementCard/elementCard";
 import License from "./misc/license";
 import SourceCode from "./misc/sourceCode";
-import Sidebar from "./sidebar/Sidebar"; // <-- ts is ragebaiting me
+import Sidebar from "./sidebar/sidebar"; // <-- ts is ragebaiting me
 
 export { MainPage, Sidebar, PeriodicTable, ElementCard, License, SourceCode };
